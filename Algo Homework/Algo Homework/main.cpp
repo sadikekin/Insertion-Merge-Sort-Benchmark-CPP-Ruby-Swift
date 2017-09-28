52f4d29cdb6775e06041a53ce6edf60e45b6121c
Sadik Ekin Ozbay <sadikekinozbay@gmail.com>
2017-09-28 17:04:48 +0300

    core

---
+++ Algo Homework/Algo Homework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algo Homework
+//
+//  Created by Sadik Ekin Ozbay on 9/28/17.
+//  Copyright © 2017 Sadik Ekin Ozbay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
